65a2fd09c1dff2796083392f1e7dc20bc97c351c
Angie <angiejimenez@gmail.com>
2023-08-19 06:30:05 +0200

    PrivateRoute and PublicRoute

--- heroes-spa/src/router/AppRotuer.jsx
+++ heroes-spa/src/router/AppRotuer.jsx
@@ -1,15 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { HeroesRoutes } from "../heroes/";
 import { LoginPage } from "../auth/"
+import { PriveteRoute } from "./PriveteRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRotuer = () => {
   return (
     <>  
 
     <Routes>
-    <Route path="login" element={<LoginPage />} />        
-        <Route path="/*" element={<HeroesRoutes />} />
+    
+    <Route path="login/*" element={
+      <PublicRoute>
+      <Routes>
+        <Route path="/*" element={<LoginPage />} />  
+      </Routes>
+      </PublicRoute>
+    } />
+   
 
+      <Route path="/*" element={
+      <PriveteRoute>
+        <HeroesRoutes />
+      </PriveteRoute>
+      } />
     </Routes>
     </>
   )
